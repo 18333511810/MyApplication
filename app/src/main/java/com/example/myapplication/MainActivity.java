fcae14bfe20406c26499428a732304b07746c848
18333511810 <252628196@qq.com>
2019-10-09 09:56:30 +0800

    点击按钮

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button mybut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mybut = (Button) findViewById(R.id.button);
+        mybut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mybut.setText("按钮已被点击");
+            }
+        });
+
     }
 }
